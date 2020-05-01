786bb446c9117c4118caf6c0d1b13f28a752ca0f
spujari <spujari@spujaris-MacBook-Pro.local>
2020-05-01 08:32:36 -0700

    Added copyright for files

--- ACPAnalytics/Assets/Plugins/ACPAnalytics.cs
+++ ACPAnalytics/Assets/Plugins/ACPAnalytics.cs
@@ -1,4 +1,15 @@
-﻿using System.Collections;
+﻿/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,9 +27,9 @@ namespace com.adobe.marketing.mobile
 			redirectedDelegate = callback;
 		}
 
-		void call(long status)
+		void call(long size)
 		{
-			redirectedDelegate (status);
+			redirectedDelegate (size);
 		}
 	}
 
