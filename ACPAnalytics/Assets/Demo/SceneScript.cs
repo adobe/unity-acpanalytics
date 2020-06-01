0be7219bd7df20fed1fb23f4e60a34c4941afad9
ravinsingh jain <ravjain@adobe.com>
2020-06-01 10:19:03 -0700

    Adding MIT license in demo dir and adding notices

--- ACPAnalytics/Assets/Demo/SceneScript.cs
+++ ACPAnalytics/Assets/Demo/SceneScript.cs
@@ -5,7 +5,11 @@ NOTICE: Adobe permits you to use, modify, and distribute this file in
 accordance with the terms of the Adobe license agreement accompanying
 it. If you have received this file from a source other than Adobe,
 then your use, modification, or distribution of it requires the prior
+<<<<<<< HEAD
 written permission of Adobe.
+=======
+written permission of Adobe. (See LICENSE-MIT for details)
+>>>>>>> eda3c41... Adding MIT license in demo dir and adding notices
 */
 
 using System.Collections;
